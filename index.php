691e9a1899ad6e97de963dd7c56a894e119d1c8b
swongwc3 <sharonw@wc-3.com>
2015-06-15 17:20:05 -0700

    time

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<html>
+<head>
+
+</head>
+
+<body>
+
+
 <?php
 /**
  * Created by PhpStorm.
@@ -6,5 +14,25 @@
  * Time: 2:57 PM
  */
 
-$date = new DateTime();
-echo 'Current date: ' . $date->format ('m/d/y') . "\n";
+date_default_timezone_set('America/Los_Angeles');
+
+echo '<b>Currently: ' . date('F j, Y h:i:sa P') . 'GMT</b></br>';
+$var = 0;
+echo '<a href="#">'.$var.'</a>';
+
+
+/*
+ * http://php.net/manual/en/function.date.php
+ * 'm/d/y' - 06/15/2015
+ * 'm/d/Y' - 06/15/2015
+ * 'H:i:s' - 00:21:45
+ * 'g:i A' - 5:45 PM
+ * 'G:ia' - 05:45pm
+ * '\o\n l jS F Y' - on Saturday 24th March 2012
+ * j - day (jS 24th)
+ * F - month (march)
+ * l - day of week (Saturday)
+ */
+?>
+
+</body></html>
