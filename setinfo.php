19226f02b6ff6913d5cb5ceb5d1f57f2bcb335f1
swongwc3 <sharonw@wc-3.com>
2015-06-18 10:12:57 -0700

    stuff

--- setinfo.php
+++ setinfo.php
@@ -11,7 +11,7 @@ include_once('db.php');
 DB::connection();
 
 $results = DB::sql('SELECT * FROM Items');
-
+var_dump($results);
 foreach ($results as $piece) {
     foreach ($piece as $key => $value) {
         if ($value != null) {
